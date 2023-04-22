47d2ffbfa47582e05d4c1b3d735ac4ddf8c0307c
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2023-04-22 21:11:00 +1200

    Moved routes to index file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import { Navbar, Breadcrumb } from "./components/Components";
-import { Contact, Products, Home, Company, NotFound } from "./pages/Pages";
-import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Navbar />
       <Breadcrumb />
-      <Routes>
-        <Route path="/" index element={<Home />} />
-        <Route path="/pages/products" element={<Products />} />
-        <Route path="/pages/company" element={<Company />} />
-        <Route path="/pages/contact" element={<Contact />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </div>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Contact, Products, Home, Company, NotFound } from "../src/pages/Pages";
 import "./scss/sitebase.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -9,6 +10,13 @@ root.render(
   <BrowserRouter>
     <React.StrictMode>
       <App />
+      <Routes>
+        <Route path="/" index element={<Home />} />
+        <Route path="/pages/products" element={<Products />} />
+        <Route path="/pages/company" element={<Company />} />
+        <Route path="/pages/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </React.StrictMode>
   </BrowserRouter>
 );
