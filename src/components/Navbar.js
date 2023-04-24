f566577ce674fa0232def852fef29eff68007d31
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2023-04-25 11:57:18 +1200

    Code cleanup for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,26 +4,24 @@ import { routes } from "../Routes";
 
 export const Navbar = () => {
   return (
-    <div className="navbar-wrapper">
-      <nav className="navbar">
-        <ul className="options">
-          {routes.map((route, key) => {
-            return (
-              <li key={key}>
-                <NavLink
-                  to={route.path}
-                  className={({ isActive }) => (isActive ? "active" : "")}
-                >
-                  {route.title}
-                </NavLink>
-              </li>
-            );
-          })}
-          <li>
-            <input className="search-bar" type="text" placeholder="Search..." />
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <nav>
+      <ul>
+        {routes.map((route, key) => {
+          return (
+            <li key={key}>
+              <NavLink
+                to={route.path}
+                className={({ isActive }) => isActive && "active"}
+              >
+                {route.title}
+              </NavLink>
+            </li>
+          );
+        })}
+        <li>
+          <input className="search-bar" type="text" placeholder="Search..." />
+        </li>
+      </ul>
+    </nav>
   );
 };
