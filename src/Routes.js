c959e3c1b357e75fe0d6beaa6bd41f6996057773
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2023-10-13 13:23:52 +1300

    Delete routes and files

--- src/Routes.js
+++ src/Routes.js
@@ -1,19 +1,8 @@
-import {
-  Company,
-  Home,
-  NotFound,
-  Products,
-  ProductDetails,
-} from "./pages/index";
+import { Home, NotFound, Products } from "./pages/index";
 
 export const routes = [
   { path: "/", component: <Home />, title: "Home" },
   { path: "/products", component: <Products />, title: "Products" },
-  { path: "/company", component: <Company />, title: "About" },
-  {
-    path: "/products/:id",
-    component: <ProductDetails />,
-    title: "",
-  },
+
   { path: "*", component: <NotFound /> },
 ];
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
 import { Products } from "./Products";
-import { Home } from "./Home";
+import Home from "./Home";
 import { NotFound } from "./NotFound";
-import { Company } from "./Company";
-import { ProductDetails } from "./ProductDetails";
 
-export { Products, Home, NotFound, Company, ProductDetails };
+export { Products, Home, NotFound };
