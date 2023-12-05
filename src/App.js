481ad6e07d485e0b75763f785a6ae7e8191f9ca0
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2023-12-05 14:41:16 +1300

    Wipe files

--- src/App.js
+++ src/App.js
@@ -1,18 +1,5 @@
-import Navbar from "./components";
-import { routes } from "./Routes";
-import { Routes, Route } from "react-router-dom";
-
 function App() {
-  return (
-    <>
-      <Navbar routes={routes} />
-      <Routes>
-        {routes.map(({ path, component }, key) => (
-          <Route path={path} element={component} key={key} />
-        ))}
-      </Routes>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
