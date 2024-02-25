45e4909f540eb51e6e9798ea5de3b9e57302162e
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2024-02-25 22:15:58 +1300

    Delete theme from index file

--- src/index.js
+++ src/index.js
@@ -1,52 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./scss/index.scss";
-import { createTheme, ThemeProvider } from "@mui/material";
+import "./index.scss";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-const theme = createTheme({
-  typography: {
-    fontFamily: "Inter",
-    h1: {
-      paddingBottom: 12,
-    },
-  },
-  components: {
-    MuiButton: {
-      variants: [
-        {
-          props: { variant: "special" },
-          style: {
-            background: "none",
-            color: "#000",
-            "&:hover": {
-              transition: "none",
-              background: "none",
-              color: "inherit",
-              boxShadow: "none",
-            },
-          },
-        },
-      ],
-      styleOverrides: {
-        root: {
-          backgroundColor: "#000",
-          borderRadius: 0,
-          boxShadow: "none",
-          color: "#fff",
-          "&:hover": {
-            transition: "none",
-            backgroundColor: "#000",
-            boxShadow: "none",
-            color: "#fff",
-          },
-        },
-      },
-    },
-  },
-});
 
 root.render(
   <React.StrictMode>
