27ea6705e72709ebee67d38951062f075b2ebed6
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2024-04-03 21:25:18 +1300

    Create routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import Cart from "./pages/Cart";
+import Products from "./pages/Products";
+import ProductDetails from "./pages/ProductDetails";
+import { createContext, useState } from "react";
+
 function App() {
-  return <></>;
+  const Layout = () => {
+    return (
+      <>
+        <Navigation />
+        <Outlet />
+      </>
+    );
+  };
+
+  return (
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Products />} />
+            <Route path="/:id/:name" element={<ProductDetails />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<>Error Page</>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+  );
 }
 
 export default App;
