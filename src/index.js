02dad8585b6fda836c9686e2518d51dbcd35295c
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2024-06-09 19:31:24 +1200

    Set up commerce config

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./theme";
+import "./styles.scss";
+import commerce from "./commerce";
+import CssBaseline from "@mui/material/CssBaseline";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <CssBaseline />
+      <App commerce={commerce} />
     </ThemeProvider>
   </React.StrictMode>
 );
