0f5621460f21270df25de3fe8b7647a4b49ab822
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2024-06-09 19:42:12 +1200

    Carousel component for assets

---
+++ src/components/Carousel.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel as Gallery } from "react-responsive-carousel";
+import { Box } from "@mui/material";
+
+const Carousel = (product) => (
+  <Gallery autoPlay={true} swipeable={true} draggable={true}>
+    {product.assets.map((asset) => (
+      <Box>
+        <Box component="img" src={asset.url} />
+        {/* <Box component="p" className="legend">{asset.id}</Box> */}
+      </Box>
+    ))}
+  </Gallery>
+);
+
+export default Carousel;
