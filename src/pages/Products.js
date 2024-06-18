2b6f16afefe8d80bea4d353005aa522559b0c6d7
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2024-06-18 15:45:11 +1200

    Code cleaning, miscs

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,107 +1,194 @@
+import { useState, useEffect } from "react";
+import { Box } from "@mui/material";
 import ProductList from "../components/ProductList";
+import Loading from "../components/Loading";
+import commerce from "../commerce";
+import Select from "../components/Select";
+import ExpandMoreIcon from "@mui/icons-material/ExpandLess";
+import ExpandLessIcon from "@mui/icons-material/ExpandMore";
 
 function Products() {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState({});
+  const [value, setValue] = useState("a-z");
+  const [categoryInput, setCategoryInput] = useState("");
+  const [openedSort, setOpenedSort] = useState(false);
+  const [openedCategory, setOpenedCategory] = useState(false);
+  const [search] = useState("");
+
+  const sortByLowest = (products) =>
+    products.sort(function (a, b) {
+      return a.price.raw - b.price.raw;
+    });
+
+  const sortByHighest = (products) =>
+    products.sort(function (a, b) {
+      return b.price.raw - a.price.raw;
+    });
+
+  const sortAZ =
+    value === "a-z" && products.sort((a, b) => a.name.localeCompare(b.name));
+
+  const sortZA =
+    value === "z-a" && products.sort((a, b) => b.name.localeCompare(a.name));
+
+  if (value === "lowest") {
+    sortByLowest(products);
+  }
+  if (value === "highest") {
+    sortByHighest(products);
+  }
+
+  const searchResults = products.filter((product) =>
+    product.name.toLowerCase().includes(search)
+  );
+
+  const productCategory = products.filter(
+    (product) => product.categories[0].slug === categoryInput
+  );
 
   useEffect(() => {
-    async function fetchProducts() {
-      commerce.products
-        .list()
-        .then((product) => {
-          setProducts(product.data);
-          setLoading(false);
-        })
-        .catch((error) => {
-          setError(error);
-          console.log({ error });
-        });
-    }
+    commerce.products
+      .list()
+      .then((res) => {
+        setProducts(res.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-    fetchProducts();
-  }, [products]);
-
-  if (error?.length > 0) return <>{error.message}</>;
+  if (loading) return <Loading />;
 
   return (
     <Box
-      container
-      sx={{
-        display: "grid",
-        columnGap: "10px",
-        rowGap: "15px",
-        padding: "16px",
-        margin: "100px auto auto auto",
-        width: { xs: "100%", sm: "85%", md: "85%", lg: "85%" },
-        gridTemplateColumns: {
-          xs: "repeat(2, 1fr)",
+      padding={{
+        xs: "46px 24px",
+        sm: "46px 48px",
+        md: "46px 48px",
+        lg: "46px 48px",
+      }}
+      fontSize={14}
+    >
+      <Box
+        paddingTop="24px"
+        paddingBottom="48px"
+        display="flex"
+        flexDirection={{ xs: "row", sm: "row", md: "row", lg: "row" }}
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Box
+          sx={{
+            width: "100%",
+            display: "flex",
+            flexDirection: { xs: "column", sm: "row" },
+            gap: { xs: 4, sm: 16 },
+          }}
+        >
+          <Box
+            sx={{
+              position: "relative",
+            }}
+          >
+            <Box
+              component="span"
+              sx={{ display: "block", fontWeight: 600 }}
+              onClick={() => setOpenedSort((opened) => !opened)}
+            >
+              SORT{" "}
+              {openedSort ? (
+                <ExpandMoreIcon sx={{ verticalAlign: "middle" }} />
+              ) : (
+                <ExpandLessIcon sx={{ verticalAlign: "middle" }} />
+              )}
+            </Box>
+            {openedSort && (
+              <Box
+                sx={{
+                  position: {
+                    xs: "relative",
+                    sm: "absolute",
+                    md: "absolute",
+                    lg: "absolute",
+                  },
+                }}
+              >
+                <Select
+                  onChange={(e) => setValue(e.target.value)}
+                  value={value}
+                  options={[
+                    { value: "lowest", name: "Price: Low to High" },
+                    { value: "highest", name: "Price: High to Low" },
+                    { value: "a-z", name: "A-Z" },
+                    { value: "z-a", name: "Z-A" },
+                  ]}
+                />
+              </Box>
+            )}
+          </Box>
+          <Box
+            sx={{
+              position: "relative",
+            }}
+          >
+            <Box
+              component="span"
+              sx={{ display: "block", fontWeight: 600 }}
+              onClick={() => setOpenedCategory((category) => !category)}
+            >
+              CATEGORY{" "}
+              {openedCategory ? (
+                <ExpandMoreIcon sx={{ verticalAlign: "middle" }} />
+              ) : (
+                <ExpandLessIcon sx={{ verticalAlign: "middle" }} />
+              )}
+            </Box>
+            {openedCategory && (
+              <Box
+                sx={{
+                  position: {
+                    xs: "relative",
+                    sm: "absolute",
+                    md: "absolute",
+                    lg: "absolute",
+                  },
+                }}
+              >
+                <Select
+                  value={categoryInput}
+                  onChange={(e) => setCategoryInput(e.target.value)}
+                  options={[
+                    { value: "", name: "All" },
+                    { value: "bottoms", name: "Bottoms" },
+                    { value: "hoodies", name: "Hoodies" },
+                    { value: "accessories", name: "Accessories" },
+                    { value: "t-shirts", name: "T-Shirts" },
+                  ]}
+                />
+              </Box>
+            )}
+          </Box>
+        </Box>
+      </Box>
+      <Box
+        container
+        display="grid"
+        columnGap="15px"
+        rowGap="30px"
+        gridTemplateColumns={{
+          xs: "repeat(1, 1fr)",
           sm: "repeat(2, 1fr)",
           md: "repeat(3, 1fr)",
           lg: "repeat(4, 1fr)",
-        },
-      }}
-    >
-      {loading ? (
-        <CircularProgress thickness={2} sx={{ color: "black" }} size="12rem" />
-      ) : (
-        <ProductsList products={products} />
-      )}
+        }}
+      >
+        <ProductList
+          products={productCategory.length === 0 ? products : productCategory}
+        />
+      </Box>
     </Box>
-  );
-}
-
-function ProductsList({ products }) {
-  return (
-    <>
-      {products.map((product) => (
-        <Box item key={product.id}>
-          <Link
-            to={`/${product.id}/${product.name}`}
-            style={{ color: "inherit", textDecoration: "none" }}
-          >
-            <Box
-              component={"img"}
-              sx={{ userSelect: "none", width: "100%" }}
-              src={product.image.url}
-              alt={product.name}
-            />
-            <Box
-              sx={{
-                color: "black",
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "space-between",
-                padding: "16px 0",
-                gap: 1,
-              }}
-            >
-              <Typography
-                component="h4"
-                variant="h4"
-                sx={{
-                  color: "black",
-                  fontWeight: 700,
-                  fontSize: { xs: 14, sm: 16, md: 16, lg: 16 },
-                  textTransform: "uppercase",
-                }}
-              >
-                {product.name}
-              </Typography>
-              <Typography
-                component="span"
-                sx={{
-                  color: "black",
-                  fontSize: { xs: 14, sm: 16, md: 16, lg: 16 },
-                }}
-              >
-                {product.price.formatted_with_code}
-              </Typography>
-            </Box>
-          </Link>
-        </Box>
-      ))}
-    </>
   );
 }
 
