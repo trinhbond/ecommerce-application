ade2ab4cc08cda8d8c1c2807564d3a73d55a3cdf
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2024-06-21 20:03:07 +1200

    Search for a product

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,40 @@
 import Box from "@mui/material/Box";
 import { Link } from "react-router-dom";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import CloseIcon from "@mui/icons-material/Close";
+import SearchIcon from "@mui/icons-material/Search";
+import { useEffect, useState } from "react";
+import commerce from "../commerce";
+import { TextField } from "@mui/material";
+import ProductList from "./ProductList";
 
 export default function Navigation() {
+  const [products, setProducts] = useState([]);
+  const [search, setSearch] = useState("");
+  const [isOpened, setIsOpened] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const results = products.filter((product) =>
+    product.name.toLowerCase().includes(search)
+  );
+
+  useEffect(() => {
+    commerce.products
+      .list()
+      .then((res) => {
+        setProducts(res.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log({ error });
+      });
+  }, []);
+
+  const handleChange = (e) => setSearch(e.target.value);
+
   return (
     <Box component="header">
       <Box
-        width="100%"
         padding={{
           xs: "46px 14px",
           sm: "46px 48px",
@@ -19,7 +47,6 @@ export default function Navigation() {
       >
         <Box
           sx={{
-            width: { xs: "100%", sm: "100%", md: "l00%", lg: "100%" },
             display: "flex",
             justifyContent: "space-between",
             flexDirection: "row",
@@ -27,7 +54,14 @@ export default function Navigation() {
             padding: 0,
           }}
         >
-          <Box></Box>
+          <Box>
+            <Box
+              component="span"
+              onClick={() => setIsOpened((isOpened) => !isOpened)}
+            >
+              <SearchIcon />
+            </Box>
+          </Box>
           <Link
             to="/"
             style={{
@@ -43,6 +77,62 @@ export default function Navigation() {
             </Link>
           </Box>
         </Box>
+        {isOpened && (
+          <Box
+            sx={{
+              padding: {
+                xs: "46px 14px",
+                sm: "46px 48px",
+                md: "46px 48px",
+                lg: "46px 48px",
+              },
+              background: "#ffffff",
+              position: "fixed",
+              width: "min(150ch, 100%)",
+              margin: "auto",
+              left: 0,
+              right: 0,
+              bottom: 0,
+              top: 0,
+              zIndex: 9999,
+              overflowY: "scroll",
+            }}
+          >
+            <Box paddingBottom={2}>
+              <Box
+                component="span"
+                onClick={() => setIsOpened((isOpened) => !isOpened)}
+              >
+                <CloseIcon />
+              </Box>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "flex-end",
+                borderBottom: "1px solid black",
+              }}
+            >
+              <SearchIcon sx={{ color: "action.active", mr: 1, my: 0.5 }} />
+              <TextField
+                id="input-with-sx"
+                placeholder="Search for a product..."
+                variant="standard"
+                onChange={handleChange}
+                sx={{
+                  [`& .MuiInputBase-root::before`]: { borderBottom: 0 },
+                  [`& .MuiInputBase-root::after`]: {
+                    borderBottom: 0,
+                  },
+                  ["& .MuiInput-underline:hover:before"]: {
+                    border: "none !important",
+                  },
+                }}
+              />
+            </Box>
+            {!search || (search !== "" && <ProductList products={results} />)}
+          </Box>
+        )}
       </Box>
     </Box>
   );
