b4bd9cb48136625d6018ac01eac2d15fb005999e
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2024-08-31 19:14:29 +1200

    Update icons and function

--- src/components/CartItems.js
+++ src/components/CartItems.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import commerce from "../commerce";
-import { Close } from "@mui/icons-material";
+import { IoCloseSharp } from "react-icons/io5";
 
 export default function CartItems({ cart }) {
   const [status, setStatus] = useState("");
@@ -38,12 +38,12 @@ export default function CartItems({ cart }) {
       </tr>
       {cart.line_items.map((product) => (
         <tr key={product.id}>
-          <td className="product py-4 w-3/12">
+          <td className="py-4 w-3/12">
             <div className="max-[360px]:min-w-[100px] max-[360px]:w-[100px] text-center">
               <img src={product.image.url} alt={product.name} />
             </div>
           </td>
-          <td className="product px-4 name text-start">
+          <td className="px-4 name text-start">
             <div>
               <Link to={`/${product.product_id}/${product.name}`}>
                 <span className="font-semibold">{product.name}</span>
@@ -51,7 +51,7 @@ export default function CartItems({ cart }) {
               <span className="block">
                 {product.price.formatted_with_symbol}
               </span>
-              <div className="actions hidden max-[560px]:inline-block max-[560px]:mt-3">
+              <div className="hidden max-[560px]:inline-block max-[560px]:mt-3">
                 <div
                   className={`${
                     status === "loading" && "bg-[#cccccc]"
@@ -60,7 +60,7 @@ export default function CartItems({ cart }) {
                   <button
                     className="disabled:bg-[#cccccc] font-semibold p-0 w-min-auto"
                     onClick={() =>
-                      updateQuantity(product.id, product.quantity, "remove")
+                      updateQuantity(product.id, "remove", product.quantity)
                     }
                     disabled={status === "loading"}
                   >
@@ -70,7 +70,7 @@ export default function CartItems({ cart }) {
                   <button
                     className="disabled:bg-[#cccccc] font-semibold p-0 w-min-auto"
                     onClick={() =>
-                      updateQuantity(product.id, product.quantity, "add")
+                      updateQuantity(product.id, "add", product.quantity)
                     }
                     disabled={status === "loading"}
                   >
@@ -80,7 +80,7 @@ export default function CartItems({ cart }) {
               </div>
             </div>
           </td>
-          <td className="quantity text-end max-[560px]:hidden">
+          <td className="text-end max-[560px]:hidden">
             <div
               className={`${
                 status === "loading" && "bg-[#cccccc]"
@@ -89,12 +89,7 @@ export default function CartItems({ cart }) {
               <button
                 className="disabled:bg-[#cccccc] font-semibold p-0 w-min-auto"
                 onClick={() =>
-                  updateQuantity(
-                    product.id,
-                    product.quantity,
-                    "remove",
-                    setStatus
-                  )
+                  updateQuantity(product.id, "remove", product.quantity)
                 }
                 disabled={status === "loading"}
               >
@@ -104,7 +99,7 @@ export default function CartItems({ cart }) {
               <button
                 className="disabled:bg-[#cccccc] font-semibold p-0 w-min-auto"
                 onClick={() =>
-                  updateQuantity(product.id, product.quantity, "add")
+                  updateQuantity(product.id, "add", product.quantity)
                 }
                 disabled={status === "loading"}
               >
@@ -112,12 +107,12 @@ export default function CartItems({ cart }) {
               </button>
             </div>
           </td>
-          <td className="subtotal w-[25%] text-end relative">
+          <td className="w-1/4 text-end relative">
             <span
-              className="absolute top-[8px] right-[-4px] cursor-pointer "
+              className="absolute top-[8px] right-[-4px] cursor-pointer"
               onClick={() => handleClick(product.id)}
             >
-              <Close sx={{ color: "#cdcdcd" }} />
+              <IoCloseSharp fontSize={24} color={"#cdcdcd"} />
             </span>
             {product.line_total.formatted_with_symbol}
           </td>
