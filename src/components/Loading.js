0bda0df1f35286a8d7f3320093129ac028a1ccb2
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2024-08-31 19:10:52 +1200

    Change spinner

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,21 +1,12 @@
-import { CircularProgress } from "@mui/material";
-
 export default function Loading() {
   return (
-    <CircularProgress
-      thickness={2}
-      size="12rem"
-      sx={{
-        color: "#000000",
-        top: 0,
-        bottom: 0,
-        left: 0,
-        right: 0,
-        justifySelf: "center",
-        alignSelf: "center",
-        display: "grid",
-        position: "absolute",
-      }}
-    />
+    <div
+      className="absolute justify-self-center self-center top-0 bottom-0 left-0 right-0 h-28 w-28 animate-spin rounded-full border-8 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+      role="status"
+    >
+      <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+        Loading...
+      </span>
+    </div>
   );
 }
